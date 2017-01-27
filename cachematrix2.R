99134487884f6eac90c4b9ac15bb042f8cfec012
cbernot <cbernot2@gmail.com>
2017-01-27 16:50:42 -0500

    This is a new commit for what I originally planned to be an amendmend

--- cachematrix2.R
+++ cachematrix2.R
@@ -1,6 +1,7 @@
 ##calculating the inverse of a square matrix
-##first portion of program is to fill the cache of the matrix:
-##set a value of a matrix to NULL
+##create an object to store a cache value of the inverse
+##define functions and return a list of functions used as input into CacheSolve
+##These functions are:
 ##set the value of the matrix
 ##get the value of the matrix
 ##set the value of the inverse in cache
@@ -15,6 +16,9 @@ makeCacheMatrix <- function(x = matrix()) {
     set<-function(y){
         x<<-y
         invm<<-NULL
+          ## is used to assign a value to an object that exists in a different 
+          ##environment 
+          
     }
     
     get<-function()x
@@ -29,29 +33,35 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## return a matrix that is the inverse of "x"
+## return a matrix that is the inverse of "x" matrix input from makeCacheMatrix
 ##this function assumes that the matrix supplied is always invertible
-##access the inverse of the matrix
-##place condition that if invm is not a null value the return message "getting 
-##cached data"
-## then access the values in matrix x
-## if invm is NULL then:
-##calculate the inverse
-##now store in cache the inverse value
+
+
 ##list invm
 
 cacheSolve <- function(x, ...) {
+  ##access the inverse of the matrix
+  ##place condition that if invm is not a null value the return message "getting 
+  ##cached data"
+  ## then access the values in matrix x 
   
   invm<-x$getinverse()
   
   if(!is.null(invm)){
     message("getting cached data")
     return(invm)}
-    
+  
+  ## if invm is NULL then:
+  ##calculate the inverse
+  
+  
     matrixd<-x$get()
     invm<-solve(matrixd, ...)
+    ##now store in cache the inverse value 
+    
     
     x$setinverse(invm)
+    
     return(invm)
     
   }
@@ -59,7 +69,7 @@ cacheSolve <- function(x, ...) {
 
 
 
-    
+ ##I have included matrix values to test makeCacheMatrix and cacheSolve   
     
 a<-makeCacheMatrix()
 a$set(matrix(1:4,2,2))
